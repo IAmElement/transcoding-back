df6643b844d25a7112e8b6fcfd422901a3d679c0
Element <97992918+IAmElement@users.noreply.github.com>
2023-01-29 22:01:22 +1100

    (feat): try to push frames to mmdvm?
    mmdvm doesn't really know what to do with them does it

--- transcode.sh
+++ transcode.sh
@@ -5,7 +5,9 @@ for file in /data/mp3/*.mp3; do
   if [ -f "$file" ]; then
     # Transcode the file into P25 frames
     ffmpeg -i "$file" -c:a pcm_s16le -ar 48000 -f mulaw output.p25
-    # Move the resulting P25 frames to the dump directory
-    mv output.p25 /data/p25/
+    # Send the P25 frames to the MMDVM board for transmission
+    cat output.p25 > /dev/ttyACM0
+    # Move the processed file to the archive directory
+    mv "$file" /data/mp3/archive/
   fi
 done
