04c1655718727f44fbc7e82e1181b60a2563c703
Element <97992918+IAmElement@users.noreply.github.com>
2023-01-29 22:14:54 +1100

    (feat): rename file + don't try and delete before moving

--- transcode.sh
+++ transcode.sh
@@ -7,9 +7,9 @@ for file in /data/mp3/*.mp3; do
     ffmpeg -i "$file" -c:a pcm_s16le -ar 48000 -f mulaw output.p25
     # Send the P25 frames to the MMDVM board for transmission
     cat output.p25 > /dev/ttyACM0
-    # Delete the processed file
-    rm output.p25
+    # Rename the processed file to the current date/time
+    filename=$(date +"%Y-%m-%d_%H-%M-%S").mp3
     # Move the processed file to the archive directory
-    mv "$file" /data/mp3/archive/
+    mv "$file" /data/mp3/archive/$filename
   fi
 done
